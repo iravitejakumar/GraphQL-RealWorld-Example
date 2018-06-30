2eee78b50bfd83760e495733d6723a2efed2a937
iravitejakumar <iravitejakumar@gmail.com>
2018-07-01 01:54:28 +0530

    Imported Schema and GraphQL works

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 23 },
+  { id: '47', firstName: 'Samantha', age: 21 }
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -16,7 +22,13 @@ const RootQuery = new GraphQLObjectType({
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
-      resolve(parentValue, args) {}
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
     }
   }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
